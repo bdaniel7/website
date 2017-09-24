5cdb21ff11295b6eca330a7f3ab282703f671ee8
bdaniel7 <bdaniel7@gmail.com>
2017-09-24 10:40:43 +0300

    Added bootstrap

--- botez/app.js
+++ botez/app.js
@@ -1,8 +1,12 @@
-import SingleFileComponent from './SingleFileComponent.js';
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(BootstrapVue);
+
 
 new Vue({
-  el: '#app',
-  components: {
-    SingleFileComponent
-  }
+  el: '#app'
 });
